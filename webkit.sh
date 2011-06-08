53c782f1519609e3061892606dbe1137be6cfa63
Julien Chaffraix <jchaffraix@google.com>
2011-06-08 13:23:05 -0700

    Added a WebKit specific script file.

---
+++ webkit.sh
@@ -0,0 +1,19 @@
+function setup_WebKit() {
+    # Qt for now.
+    export PATH=$HOME/Deps/bin:$PATH
+    export QTDIR=$HOME/Deps/
+
+    # Needed by WebKit to run DRT.
+    # FIXME: Qt specific?
+    xhost +local: > /dev/null 2>&1
+
+    ### Webkit Info
+    export CHANGE_LOG_NAME="Julien Chaffraix"
+    export EMAIL_ADDRESS="jchaffraix@webkit.org"
+
+    # FIXME: Should be conditional!!!
+    export WEBKIT_ROOT="$HOME/Sources/WebKit"
+    export SVN_EDITOR="$WEBKIT_ROOT/Tools/Scripts/commit-log-editor"
+
+    cd $WEBKIT_ROOT
+}
