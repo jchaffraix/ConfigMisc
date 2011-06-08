a870630364c60dc54afdc3ee2ffc371b8d776bd0
Julien Chaffraix <jchaffraix@google.com>
2011-06-08 13:17:47 -0700

    Started to work on init scripts for screen. WIP.

---
+++ screen/setup_common.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+# Source our usual conf.
+# FIXME: Better way?
+source /etc/profile
+source ~/.bashrc
+
+echo "Foo"
---
+++ screen/setup_webkit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+# FIXME: Hardcoded!
+source ~/.screen/setup_common.sh
+
+echo "Bar"
+
+setup_WebKit
