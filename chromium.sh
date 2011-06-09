90806bf2e377d7824443d36b1acd51ba9eb78359
Julien Chaffraix <jchaffraix@google.com>
2011-06-08 19:19:34 -0700

    Added some Chromium files.
    Some building + setup function and the screen equivalent.

---
+++ chromium.sh
@@ -0,0 +1,29 @@
+function logical_core_nums()
+{
+    echo `cat /proc/cpuinfo|grep processor|wc -l`
+}
+
+function setup_Chromium()
+{
+    export CHROMIUM_ROOT=$HOME/Sources/Chromium/src
+    cd $CHROMIUM_ROOT
+}
+
+function build_chromium()
+{
+    if [ ! -d "build" ]
+    then
+        echo "Not in the Chromium root!"
+        return
+    fi
+
+    if [[ $OSTYPE =~ "darwin" ]]
+    then
+        pushd build/
+        xcodebuild -project all.xcodeproj -configuration Debug -target All
+        popd
+    elif [[ $OSTYPE =~ "linux" ]]
+    then
+        make -j$(logical_core_nums)
+    fi
+}
---
+++ screen/setup_chromium.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+# FIXME: Hardcoded to .screen.
+source ~/.screen/setup_common.sh
+
+setup_Chromium
