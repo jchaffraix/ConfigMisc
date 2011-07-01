51bc475856523a1c9b8eb95232cdd80e267c9a06
Julien Chaffraix <jchaffraix@google.com>
2011-07-01 15:55:17 -0700

    Imported notify.pl from http://code.google.com/p/irssi-libnotify.
    It works pretty well and would be a nice base.

--- irssi/notify.pl
+++ irssi/notify.pl
@@ -1,35 +1,77 @@
-# Notification system
+##
+## Put me in ~/.irssi/scripts, and then execute the following in irssi:
+##
+##       /load perl
+##       /script load notify
+##
 
 use strict;
-use vars qw($VERSION %IRSSI);
-
 use Irssi;
+use vars qw($VERSION %IRSSI);
+use HTML::Entities;
 
-$VERSION='1.00';
-
+$VERSION = "0.01";
 %IRSSI = (
-    authors     => 'Julien Chaffraix',
-    contact     => 'jchaffraix@chromium.org',
-    name        => 'Notification',
-    description => 'Notify when you name is mentioned',
-    license     => 'Public Domain',
+    authors     => 'Luke Macken, Paul W. Frields',
+    contact     => 'lewk@csh.rit.edu, stickster@gmail.com',
+    name        => 'notify.pl',
+    description => 'Use libnotify to alert user to hilighted messages',
+    license     => 'GNU General Public License',
+    url         => 'http://lewk.org/log/code/irssi-notify',
 );
 
-# FIXME: This methods needs a lot more testing!
-sub notify {
-    my ($server, $msg, $nick, $address, $target) = @_;
-    my $myNick = $server->{nick};
+Irssi::settings_add_str('notify', 'notify_icon', 'gtk-dialog-info');
+Irssi::settings_add_str('notify', 'notify_time', '5000');
 
-    return if ($nick =~ $myNick);
-
-    if (!$target or $target =~ $myNick) {
-        my $summary = $target ? $target : $nick;
-        my $body = $msg;
-        exec("notify-send -t 1000 $summary $body");
-    }
+sub sanitize {
+  my ($text) = @_;
+  encode_entities($text);
+  return $text;
 }
 
-# Is 'print text' a better alternative to those 2
-# messages? ($dest [$dest->{server}], $text, $stripped)
-Irssi::signal_add('message public', 'notify');
-Irssi::signal_add('message private', 'notify');
+sub notify {
+    my ($server, $summary, $message) = @_;
+
+    # Make the message entity-safe
+    $summary = sanitize($summary);
+    $message = sanitize($message);
+
+    my $cmd = "EXEC - notify-send" .
+        " -i " . Irssi::settings_get_str('notify_icon') .
+        " -t " . Irssi::settings_get_str('notify_time') .
+        " -- '" . $summary . "'" .
+        " '" . $message . "'";
+
+    $server->command($cmd);
+}
+ 
+sub print_text_notify {
+    my ($dest, $text, $stripped) = @_;
+    my $server = $dest->{server};
+
+    return if (!$server || !($dest->{level} & MSGLEVEL_HILIGHT));
+    my $sender = $stripped;
+    $sender =~ s/^\<.([^\>]+)\>.+/\1/ ;
+    $stripped =~ s/^\<.[^\>]+\>.// ;
+    my $summary = $dest->{target} . ": " . $sender;
+    notify($server, $summary, $stripped);
+}
+
+sub message_private_notify {
+    my ($server, $msg, $nick, $address) = @_;
+
+    return if (!$server);
+    notify($server, "Private message from ".$nick, $msg);
+}
+
+sub dcc_request_notify {
+    my ($dcc, $sendaddr) = @_;
+    my $server = $dcc->{server};
+
+    return if (!$dcc);
+    notify($server, "DCC ".$dcc->{type}." request", $dcc->{nick});
+}
+
+Irssi::signal_add('print text', 'print_text_notify');
+Irssi::signal_add('message private', 'message_private_notify');
+Irssi::signal_add('dcc request', 'dcc_request_notify');
